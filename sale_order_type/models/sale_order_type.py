9d8e38280b760fd0911a6e07c104447d5f7af1d0
FrankiNarvaez <frannki03@gmail.com>
2025-04-09 01:52:34 -0500

    delete

--- sale_order_type/models/sale_order_type.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class SaleOrderType(models.Model):
-    _name = 'sale.order.type'
-    _description = 'Sale Order Type'
-
-    name = fields.Char(required=True)
-    code = fields.Char(required=True)
